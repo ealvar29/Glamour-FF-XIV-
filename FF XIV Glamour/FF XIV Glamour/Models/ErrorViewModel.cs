df78c2e07ea5db2f642c8204e6e6cf4036b90420
Eduardo <edalvarez2015@gmail.com>
2020-12-03 18:17:26 -0600

    Initial Project Commit

---
+++ FF XIV Glamour/FF XIV Glamour/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FF_XIV_Glamour.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
